9e20118b09e4b1be2a542d54ba82fe7f56acc88e
Valentin Kuznetsov <vkuznet@gmail.com>
2025-02-13 09:11:49 -0500

    Serve files from data location

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	srvConfig "github.com/CHESSComputing/golib/config"
 	"github.com/gin-gonic/gin"
@@ -27,8 +28,25 @@ func DataLocationHandler(c *gin.Context) {
 	for _, attr := range srvConfig.Config.CHESSMetaData.DataLocationAttributes {
 		if val, ok := meta[attr]; ok {
 			// if location attribute is found redirect to it
-			location := val.(string)
-			c.File(location)
+			dataLocation := val.(string)
+			// if location is individual file
+			//             c.File(dataLocation)
+
+			// If location is a directory, list its contents
+			files, err := os.ReadDir(dataLocation)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "unable to list directory contents"})
+				return
+			}
+
+			// Collect file names
+			var fileNames []string
+			for _, file := range files {
+				fileNames = append(fileNames, file.Name())
+			}
+
+			c.JSON(http.StatusOK, gin.H{"directory": dataLocation, "files": fileNames})
+			return
 		}
 	}
 	c.JSON(http.StatusNotFound, gin.H{"error": "data location not found in metadata"})
